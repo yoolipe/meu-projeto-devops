d3e06e5608ff10469e2bc32edc26b34f0cea7a77
yoolipe <yoolipe@hotmail.com>
2025-09-13 12:00:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Aplicacao rodando no Docker! teste PR"
+    return "Aplicacao rodando no Docker! TESTEEEEEEE"
 
 @app.route('/hello')
 def hello():
