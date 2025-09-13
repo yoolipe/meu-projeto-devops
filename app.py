b1603c0dbf4968e2d1b9bd7c3cdb8504d8c3abcb
yoolipe <yoolipe@hotmail.com>
2025-09-13 11:14:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
-"print('Ol , DevOps!')" 
-"def soma(a,b): return a+b" 
-"def sub(a,b): return a-b" 
-"print('Nova funcionalidade')" 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Aplicação rodando no Docker!"
+
+if __name__ == "__main__":
+    # Importante: host 0.0.0.0 para ser acessível de fora do container
+    app.run(host="0.0.0.0", port=5000)
